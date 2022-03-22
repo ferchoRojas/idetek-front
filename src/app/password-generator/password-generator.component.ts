074657daf668956a064db4d84cfde313cc997956
Fernando Rojas <frojas@monchis.com.py>
2022-03-22 01:30:25 +0000

    fix: password generato

--- src/app/password-generator/password-generator.component.ts
+++ src/app/password-generator/password-generator.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { Clipboard } from '@angular/cdk/clipboard';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Md5 } from 'ts-md5/dist/md5';
+import * as bcrypt from 'bcryptjs';
 
 @Component({
   selector: 'app-password-generator',
@@ -12,13 +14,21 @@ export class PasswordGeneratorComponent implements OnInit {
 
   constructor(private title: Title, private meta: Meta, private clipboard: Clipboard) { }
 
-  defaultLenght = localStorage.getItem('length')
+  defaultLength = localStorage.getItem('length')
+  defaultRounds = localStorage.getItem('rounds')
+  roundsEnabled = true
+  showPasswordMessage = false
+  showHashMessage = false
+  strongPassword = ''
+  hashPassword = ''
 
   reactiveForm = new FormGroup({
     length: new FormControl(''),
     uppercase: new FormControl(''),
     lowercase: new FormControl(''),
-    symbols: new FormControl('')
+    symbols: new FormControl(''),
+    hash: new FormControl(''),
+    rounds: new FormControl({value: '', disabled: true}, [])
   })
 
   ngOnInit(): void {
@@ -28,10 +38,12 @@ export class PasswordGeneratorComponent implements OnInit {
     
     // Set default values 
     this.reactiveForm.setValue({
-      length: this.defaultLenght ? parseInt(this.defaultLenght) : 12,
+      length: this.defaultLength ? parseInt(this.defaultLength) : 12,
       uppercase: true,
       lowercase: true,
-      symbols: true
+      symbols: true,
+      hash: '0',
+      rounds: this.defaultRounds ? parseInt(this.defaultRounds) : 10
     })
     // Generate password
     this.generatePassword()
@@ -57,16 +69,52 @@ export class PasswordGeneratorComponent implements OnInit {
     this.reactiveForm.get('symbols')?.valueChanges.subscribe(() => {
       this.generatePassword()
     })
+    this.reactiveForm.get('hash')?.valueChanges.subscribe((value) => {
+      if (value == 2) {
+        this.reactiveForm.get('rounds')?.enable()
+        this.hashPassword = this.bcryptHash(this.strongPassword, 10)
+      } else {
+        this.reactiveForm.get('rounds')?.disable()
+        this.hashPassword = this.md5Hash(this.strongPassword)
+      }
+    })
+    this.reactiveForm.get('rounds')?.valueChanges.subscribe((x) => {
+      if (!(/[0-9]/g).test(x) || x > 12) {
+        this.reactiveForm.get('rounds')?.setValue(12)
+      }
+      if (x < 6) {
+        this.reactiveForm.get('rounds')?.setValue(6)
+      }
+      if (x > 5 && x <= 12) {
+        localStorage.setItem('rounds', String(x))
+        this.hashPassword = this.bcryptHash(this.strongPassword, x)
+      }
+    })
   }
-  showMessage = false
-  strongPassword = ''
 
+  md5Hash(password: string): string {
+    return Md5.hashStr(password)
+  }
 
-  copy(): void {
-    this.showMessage = true
-    this.clipboard.copy(this.strongPassword)
+  bcryptHash(password: string, salt: number): string {
+    const rounds = bcrypt.genSaltSync(salt);
+    return bcrypt.hashSync(password, rounds);
+  }
+
+  copy(value: string): void {
+    if (value === 'hash') {
+      this.clipboard.copy(this.hashPassword)
+      this.showHashMessage = true
+    } else if (value === 'pass') {
+      this.clipboard.copy(this.strongPassword)
+      this.showPasswordMessage = true
+    }
     setTimeout(() => {
-      this.showMessage = false
+      if (value === 'hash') {
+        this.showHashMessage = false
+      } else if (value === 'pass') {
+        this.showPasswordMessage = false
+      }
     }, 1000)
   }
 
